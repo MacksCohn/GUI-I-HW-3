aaec8b2d2a829f79dd4de0426fb64980e7f7c792
= <=>
2025-06-12 01:41:57 -0400

    Mostly done I think, table works, two error checking

---
+++ script.js
@@ -0,0 +1,34 @@
+// Max Cohn
+function Generate(id) {
+    const MAX_ELEMENTS = 300 * 300;
+    const table = document.getElementById(id);
+    table.innerHTML = "";
+    const minCol = parseInt(document.getElementById('minCol').value);
+    const maxCol = parseInt(document.getElementById('maxCol').value);
+    const minRow = parseInt(document.getElementById('minRow').value);
+    const maxRow = parseInt(document.getElementById('maxRow').value);
+    if (isNaN(minCol) || isNaN(maxCol) || isNaN(minRow) || isNaN(maxRow)) {
+        table.insertRow().insertCell().textContent = 'Invalid input';
+        return;
+    }
+    if (((maxRow - minRow) * (maxCol - minCol)) > MAX_ELEMENTS) {
+        table.insertRow().insertCell().textContent = 'Range Too Large';
+        return;
+    }
+    const firstRow = table.insertRow();
+    firstRow.insertCell();
+    for (let i = minCol; i <= maxCol; i++) {
+        firstRow.insertCell().textContent = i;
+    }
+    for (let i = minRow; i <= maxRow; i++) {
+        const row = table.insertRow();
+        row.insertCell().textContent = i;
+        for (let j = minCol; j <= maxCol; j++) {
+            const cell = row.insertCell();
+            cell.textContent = i * j;
+        }
+    }
+    // let row  = table.insertRow();
+    // let cell = row.insertCell()
+    // cell.textContent = 'test';
+}
